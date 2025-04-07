79d123236e472ee9790335fb7a4a5349d1804e7e
Chariz <santoscharizkyle@gmail.com>
2025-04-08 00:03:38 +0800

    changes in pseudocode

--- quiz_creator_code.py
+++ quiz_creator_code.py
@@ -1,4 +1,10 @@
-# collect question and answers
-# format and write the data to the file
-# ask the user if they want to add more questions or exit
-# run the program
+# ask for quiz title and description
+# open the text file to save the quiz data
+    # write the quiz title and description to the file
+    # continuously ask for questions until the user decides to stop
+        # ask the user for each question and answer options
+        # validate the answer options to ensure they are unique and not empty
+        # write the question and answer options to the file
+        # ask if the user wants to add another question or exit
+# make a main menu to create a quiz or exit the program
+# run the quiz creator function if the user chooses to create a quiz
