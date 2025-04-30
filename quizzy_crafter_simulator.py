6dcc36290c654bd5c86ab28b732ef34c7f9a5057
Chariz <santoscharizkyle@gmail.com>
2025-04-30 16:20:22 +0800

    load background screens and button images for main screen

--- quizzy_crafter_simulator.py
+++ quizzy_crafter_simulator.py
@@ -8,4 +8,16 @@ root.geometry("800x600")
 root.resizable(False, False)
 
 # Load Background Images
+start_bg = ImageTk.PhotoImage(Image.open("assets/start_bg.png").resize((800, 600)))
+create_bg = ImageTk.PhotoImage(Image.open("assets/create_bg.png").resize((800, 600)))
+take_bg = ImageTk.PhotoImage(Image.open("assets/take_bg.png").resize((800, 600)))
+
 # Load Button Images for the main screen
+def load_button_image(path):
+    img = Image.open(path).convert("RGBA")
+    return ImageTk.PhotoImage(img.resize((200, 60)))
+
+button_images = {
+    "create": load_button_image("assets/create_button.png"),
+    "take": load_button_image("assets/take_button.png"),
+}
