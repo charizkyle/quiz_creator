1889b885ec03d5325bc3b8634f0fce61ba06fb66
Chariz <santoscharizkyle@gmail.com>
2025-04-30 16:00:10 +0800

    moved images to assets folder

---
+++ quizzy_crafter_simulator.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+from PIL import Image, ImageTk
+
+# ---------- SETUP ----------
+root = tk.Tk()
+root.title("Quizzy Crafter Simulator")
+root.geometry("800x600")
+root.resizable(False, False)
+
+# Load Background Images
+# Load Button Images for the main screen
